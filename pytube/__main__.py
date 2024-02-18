4d292108b73a22d283ef4973b2c2010836498bfe
pheimill <32944694+pheimill@users.noreply.github.com>
2024-02-18 10:22:39 -0500

    fixes?

--- pytube/__main__.py
+++ pytube/__main__.py
@@ -250,7 +250,7 @@ class YouTube:
     def bypass_age_gate(self):
         """Attempt to update the vid_info by bypassing the age gate."""
         innertube = InnerTube(
-            client='ANDROID_EMBED',
+            client='ANDROID',
             use_oauth=self.use_oauth,
             allow_cache=self.allow_oauth_cache
         )
@@ -474,6 +474,6 @@ class YouTube:
             The video id of the YouTube video.
 
         :rtype: :class:`YouTube <YouTube>`
-        
+
         """
         return YouTube(f"https://www.youtube.com/watch?v={video_id}")
